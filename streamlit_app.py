bc31fa60ac12f0466e426976dce0c57a1715521c
RamEklare <rameklare75@gmail.com>
2025-09-05 15:29:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ st.title("Appointment Scheduling AI Agent")
 # -----------------------------
 # Load HuggingFace LLM
 # -----------------------------
-hf_token = st.secrets["HUGGINGFACEHUB_API_TOKEN"]
+hf_token = st.secrets["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"]
 
 llm = HuggingFaceHub(
     repo_id="google/flan-t5-small",
